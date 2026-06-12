97b0f080be22ca2816be35aa5692caf8fbd3126e
261007012 <261007012@eastdelta.edu.bd>
2026-06-13 01:08:17 +0600

    Counting even and odd digits in a number.c

---
+++ Counting even and odd digits in a number.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int num, digit;
+    int evenCount = 0, oddCount = 0;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    while (num > 0) {
+        digit = num % 10;
+
+        if (digit % 2 == 0) {
+            evenCount++;
+        } else {
+            oddCount++;
+        }
+
+        num = num / 10;
+    }
+
+    printf("Even digits = %d\n", evenCount);
+    printf("Odd digits = %d\n", oddCount);
+
+    return 0;
+}
